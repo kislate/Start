637526a5244b6300a79d45f0f17f670aaca47b04
kislate <2979574787@qq.com>
2024-09-21 18:24:35 +0800

    Add files via upload

---
+++ level1-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    char name;
+    int cha, price, num;
+    printf("请分别输入你要放的货物种类、通道、货物的价钱和数量");
+    scanf_s("%c" "%d" "%d" "%d", &name, (unsigned)40, &cha, &price, &num);
+    printf("%d: ", cha);
+    int i = 1;
+    while (i <= num)
+    {
+        printf("%c", name);
+        i++;
+    }
+    printf(" %d", price);
+    return 0;
+}
